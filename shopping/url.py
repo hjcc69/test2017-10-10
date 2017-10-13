e149a4bf9e1a0cabf354a18fac1e918d3b5b7838
hjcc69git config --global user.email sml6027@naver.com <sml6027@naver.com>
2017-10-13 12:17:22 +0900

    배포3 Signed-off-by: hjcc69git config --global user.email sml6027@naver.com <sml6027@naver.com>

--- shopping/url.py
+++ shopping/url.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'product/(?P<resource_id>\d+)/$', views.show),
-    url(r'comment/create', views.comment),
+    url(r'product/(?P<resource_id>\d+)/$', views.show, name='show'),
+    url(r'comment/create', views.comment, name='comment'),
 
 
 
